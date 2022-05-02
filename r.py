8723484124ac22a047f0da05093cc0b65c35d451
zhu1017 <hs610107@hmjh.chc.edu.tw>
2022-05-02 10:37:43 +0800

    ne

--- r.py
+++ r.py
@@ -1,16 +1,16 @@
 #讀檔案
 date = []
 count = 0
-with open('orginal.txt'，'r') as f:
+with open('orginal.txt','r') as f:
     for line in f:
         date.append(line)
         count = count + 1
-        if count % == 1000:
-print(len(data))
+        if count % 1000 == 0:
+                print(len(data))
 
-print('檔案讀取玩了'，'總共有'，len(data)，'筆資料')
+print('檔案讀取玩了','總共有',len(data),'筆資料')
 
 sum_len =0
 for d in data:
 	sum_len = sum_len + len(d)
-print('平均留言長度'，sum_len/len(data))
+print('平均留言長度',sum_len/len(data))
